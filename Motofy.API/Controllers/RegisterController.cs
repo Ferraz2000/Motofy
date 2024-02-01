e3016f4abacf35763aa1e93cda49a4a40fe69bbe
Lucas Ferraz <lucas.devmoraes@gmail.com>
2024-02-01 20:59:32 -0300

    Add project files.

---
+++ Motofy.API/Controllers/RegisterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Motofy.API.Controllers
+{
+    [ApiController]
+    [Route("api/register")]
+    public class RegisterController : ControllerBase
+    {
+        
+    }
+}
